19980b9ba7b005ef2731872f391d46fe0d344793
monicakj <79156195+monicakj@users.noreply.github.com>
2021-12-19 22:55:09 +0100

    update

--- js/blog.js
+++ js/blog.js
@@ -23,6 +23,7 @@ async function fetchPosts() {
         postsContainer.innerHTML +=
         `
         <div class="posts">
+        
         <div class="posts-title">
         <a href="/specificpage.html?id=${posts[i].id}">
         <h2>${posts[i].title.rendered}</h2>
@@ -31,11 +32,10 @@ async function fetchPosts() {
         </div>
 
         <div class="posts-details">
-        <a href="/specificpage.html?id=${posts[i].id}">
-        <div class="posts-featured-img">
-        <img src="${posts[i]._embedded['wp:featuredmedia'][0].source_url}" alt="${posts[i]._embedded["wp:featuredmedia"][0].alt_text}">
+          <div class="posts-featured-img">
+            <img src="${posts[i]._embedded['wp:featuredmedia'][0].source_url}" alt="${posts[i]._embedded["wp:featuredmedia"][0].alt_text}">
+          </div>       
         </div>
-        </a>
         
         <div class="posts-text-button">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
@@ -43,13 +43,11 @@ async function fetchPosts() {
         <button class="readmore-button">Read More</button>
         </a>
         </div>
-        </div>
 
         </div>
         `;
-      }
+      } 
 
 }
 
 fetchPosts();
-
